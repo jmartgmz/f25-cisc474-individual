b8678230f78a6bceade06358e54b1d0ec016dfee
jmartgmz <jasonmartinezgmz@gmail.com>
2025-10-19 22:42:33 -0400

    change: delete old frontend

--- apps/web/app/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Footer.module.css';
-
-export default function Footer() {
-  return (
-    <footer className={styles.footer}>
-      <div>&copy; {new Date().getFullYear()} CISC474 Project.</div>
-    </footer>
-  );
-}
